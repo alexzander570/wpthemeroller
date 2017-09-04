46690c6ed0438df31903b8d4f41bb47947dfd36d
Rohit Gupta <rohit_gupta180@yahoo.co.in>
2017-09-04 09:36:42 +0530

    Update

--- wp-content/plugins/wpthemeroller/includes/wrt_page_operations.php
+++ wp-content/plugins/wpthemeroller/includes/wrt_page_operations.php
@@ -1,15 +1,20 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class wrtPageOperations{
-    public function create_plugin_pages($page_title, $page_code, $shortcodes = NULL, $template = 'default') {
+class wrtPageOperations {
+    /* Function to create page */
+
+    public function create_plugin_pages($page_title, $page_code, $shortcode_atr = NULL, $template = 'default', $is_content = false) {
         global $wpdb;
-        $the_page_name = $the_page_title = $page_title;
+
+        $the_page_title = $page_title;
+        $the_page_name = $page_title;
+
         // the menu entry...
         delete_option($page_code . "_title");
         add_option($page_code . "_title", $the_page_title, '', 'yes');
@@ -19,66 +24,75 @@ class wrtPageOperations{
         // the id...
         delete_option($page_code . "_id");
         add_option($page_code . "_id", '0', '', 'yes');
+
         $the_page = get_page_by_title($the_page_title);
+
         if (!$the_page) {
+
             // Create post object
             $_p = array();
             $_p['post_title'] = $the_page_title;
             $atr = '';
-            if (!empty($shortcodes)) {
-                $rt_shortcode = '';
-                if (is_array($shortcodes)) {
-                    foreach ($shortcodes as $shortcode) {
-                        $rt_shortcode .= '[' . $shortcode['name'];
-                        if (isset($shortcode['attr'])) {
-                            foreach ($shortcode['attr'] as $k => $v) {
-                                $rt_shortcode .= ' ' . $k . '=' . $v;
-                            }
-                        }
-                        $rt_shortcode .= ']';
+            if (!empty($shortcode_atr)) {
+                if (is_array($shortcode_atr)) {
+                    foreach ($shortcode_atr as $key => $val) {
+                        $atr .= " $key=$value";
                     }
                 } else {
-                    $rt_shortcode = '[' . $shortcodes . ']';
+                    $atr = " $shortcode_atr";
                 }
-            } else {
-                $rt_shortcode = '[' . $page_code . ']';
             }
-            $_p['post_content'] = $rt_shortcode;
+            if ($is_content) {
+                $_p['post_content'] = $atr;
+            } else {
+                $_p['post_content'] = "[" . $page_code . " $atr]";
+            }
+            if ($page_code) {
+                $_p['post_name'] = $page_code;
+            }
             $_p['post_status'] = 'publish';
             $_p['post_type'] = 'page';
             $_p['comment_status'] = 'closed';
             $_p['ping_status'] = 'closed';
-            // the default 'Uncatrgorised'
-            $_p['post_category'] = array(1);
+            $_p['post_category'] = array(1); // the default 'Uncatrgorised'
             // Insert the post into the database
             $the_page_id = wp_insert_post($_p);
         } else {
             // the plugin may have been previously active and the page may just be trashed...
+
             $the_page_id = $the_page->ID;
+
             //make sure the page is not trashed...
             $the_page->post_status = 'publish';
             $the_page_id = wp_update_post($the_page);
         }
+
         if (isset($the_page_id) && !empty($the_page_id)) {
             update_post_meta($the_page_id, '_wp_page_template', $template);
         }
+
         delete_option($page_code . "_id");
         add_option($page_code . "_id", $the_page_id);
     }
+
     /* Function to delete page */
-    public function remove_plugin_pages($page_code) {
+
+    function remove_plugin_pages($page_code) {
         global $wpdb;
         $the_page_title = get_option($page_code . "_title");
         $the_page_name = get_option($page_code . "_name");
         //  the id of our page...
-        /* get the page id */
-        $the_page_id = get_option($page_code . '_id');
+        $the_page_id = get_option($page_code . '_id');   /* get the page id */
+        if (empty($the_page_id)) {
+            $page = get_page_by_path($page_code);
+            $the_page_id = isset($page->ID) ? $page->ID : "";
+        }
         if ($the_page_id) {
-            // this will trash, not delete
-            wp_delete_post($the_page_id);
+            wp_delete_post($the_page_id, true); // this will trash, not delete
         }
         delete_option($page_code . "_title");
         delete_option($page_code . "_name");
         delete_option($page_code . "_id");
     }
+
 }
--- wp-content/plugins/wpthemeroller/index.php
+++ wp-content/plugins/wpthemeroller/index.php
@@ -3,7 +3,6 @@
 * Plugin Name: Wp Theme Roller
 * Plugin URI: 
 * Description: This is a plugin to provide a feature to customize the theme styling to the each and every user in the system. When this plugin is activated a customize button is the visible at the right side slider. Using this feature user will be able to chustomize font properties, background properties, border properties and other  basic properties.
-* Depends: Wp Page Operation 
 * Author: Rohit Gupta
 * Version: 1.0
 * Author URI: 
@@ -15,10 +14,13 @@ class wpThemeRoller{
         $this->includeFiles();
         $this->addFilters();
         $this->addShortcodes();
-        $this->createWrtPluginPages();
     }
     public function addAction(){
+        add_action( 'admin_menu', array($this,'AddAdminMenu') );
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueueStylesAndScripts' ) );
+    }
+    function AddAdminMenu() {
+	add_menu_page( 'Theme coustomizer', 'WP Theme Coustomizer', 'manage_options', 'my-unique-identifier', array($this, 'my_plugin_options'),'', 60 );
     }
     public function addFilters(){
         
@@ -31,16 +33,22 @@ class wpThemeRoller{
         include_once 'includes/add_wrt_classes.php';
         include_once 'includes/wrt_page_operations.php';
     }
-    public function createWrtPluginPages(){
-        $wrt_page_operations_obj = new wrtPageOperations();
-        $wrt_page_operations_obj->create_plugin_pages('Theme Settings', 'wrt-theme-settings', 'wrt-theme-settings');
+    static function createWrtPluginPages(){
+        $wrtPageOperations_obj = new wrtPageOperations();
+        $wrtPageOperations_obj->create_plugin_pages('Theme Settings', 'theme-settings' );
     }
-    private function addShortcodes(){
-        
+    static function removeWrtPluginPages(){
+        $wrtPageOperations_obj = new wrtPageOperations();
+        $wrtPageOperations_obj->remove_plugin_pages('theme-settings' );
+    }
+    function addShortcodes(){
+        add_shortcode( 'theme-settings' , array($this, 'my_plugin_options') );
+    }
+    function my_plugin_options(){
+        echo 'Hi';
     }
 }
 
+register_activation_hook( __FILE__, array( 'wpThemeRoller', 'createWrtPluginPages' ) );
+register_deactivation_hook( __FILE__, array( 'wpThemeRoller', 'removeWrtPluginPages' ) );
 $am = new wpThemeRoller;
-
-
-
